c71fe07ae0760619640d43eba8d4e9489d5cf140
Siman <sl5858@nyu.edu>
2021-11-19 11:36:03 -0500

    combine pages

--- js/index.js
+++ js/index.js
@@ -28,6 +28,46 @@ const itemsDataT1 = [
     seller: "EMMA BREWIN",
     price: "$39.99",
     url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+    photo: "resources/img/product1.png",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
+  },
+  {
+    name: "HAZEL BUCKET1",
+    seller: "EMMA BREWIN",
+    price: "$39.99",
+    url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+    photo: "resources/img/product2.png",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
+  },
+  {
+    name: "HAZEL BUCKET1",
+    seller: "EMMA BREWIN",
+    price: "$39.99",
+    url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+    photo: "resources/img/product1.png",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
+  },
+  {
+    name: "HAZEL BUCKET1",
+    seller: "EMMA BREWIN",
+    price: "$39.99",
+    url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+    photo: "resources/img/product2.png",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
+  },
+  {
+    name: "HAZEL BUCKET1",
+    seller: "EMMA BREWIN",
+    price: "$39.99",
+    url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+    photo: "resources/img/product1.png",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
+  },
+  {
+    name: "HAZEL BUCKET1",
+    seller: "EMMA BREWIN",
+    price: "$39.99",
+    url: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
     photo: "resources/img/product2.png",
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incididunt dolor.",
   },
@@ -265,43 +305,19 @@ function totalItemTemplate(item) {
 function itemTemplate1(item) {
   return `
         <div class="clickable detail-items">
-        <img class="item-photo" src="${item.photo}">
-        <div class="item-des"><p class="item-name">${item.name}<br>BY ${item.seller}<br>${item.price}</p>
-        <p class="item_info">${item.info}</p>
+        <img class="item-photo-detail" src="${item.photo}">
+        <div class="item-des-detail"><p class="item-name-detail">${item.name}<br>BY ${item.seller}<br>${item.price}<br> <p class="item_info">${item.info}</p></p>
+       
         <button class="purchase_btn button clickable" onclick="location.href='${item.url}'" type="button">PURCHASE</button> 
   </div>
         </div>
       `;
 }
 function itemTemplate2(item) {
-  return `
-        <div class="items clickable cardBox">
-        <img class="item-photo" src="${item.photo}">
-        <div class="item-des"><p class="item-name">${item.name}<br>BY ${item.seller}<br>${item.price}</p>
-        <button class="purchase_btn button clickable" onclick="location.href='${item.url}'" type="button">PURCHASE</button> 
-  </div>
-        </div>
-      `;
 }
 function itemTemplate3(item) {
-  return `
-        <div class="items clickable cardBox">
-        <img class="item-photo" src="${item.photo}">
-        <div class="item-des"><p class="item-name">${item.name}<br>BY ${item.seller}<br>${item.price}</p>
-        <button class="purchase_btn button clickable" onclick="location.href='${item.url}'" type="button">PURCHASE</button> 
-  </div>
-        </div>
-      `;
 }
 function itemTemplate4(item) {
-  return `
-        <div class="items clickable cardBox">
-        <img class="item-photo" src="${item.photo}">
-        <div class="item-des"><p class="item-name">${item.name}<br>BY ${item.seller}<br>${item.price}</p>
-        <button class="purchase_btn button clickable" onclick="location.href='${item.url}'" type="button">PURCHASE</button> 
-  </div>
-        </div>
-      `;
 }
 let value = document.getElementsByClassName("catalog")[0].getAttribute("value");
 console.log(value);
