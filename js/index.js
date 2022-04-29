3c8afbe1c6c08a37eb2654f77b9622e16ae82f31
Siman <sl5858@nyu.edu>
2022-04-29 08:41:39 +0800

    updates

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("index.js");
+var indexChosen=0;
+function getIndex(index){
+  console.log("index is "+index);
+  indexChosen=index;
+  return index;
+  //show login
+  // window.location.href = "psw.html"; //jump to project page
+}
