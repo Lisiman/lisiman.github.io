61527b11c2d3c4c83870c9bdd52d809dccc13066
Siman <sl5858@nyu.edu>
2022-05-28 22:40:53 +0800

    before ask fiverr

--- staging/js/index.js
+++ staging/js/index.js
@@ -1,9 +1,17 @@
 console.log("index.js");
-var indexChosen=0;
-function getIndex(index){
-  console.log("index is "+index);
-  indexChosen=index;
+var indexChosen = 0;
+function getIndex(index) {
+  console.log("index is " + index);
+  indexChosen = index;
   return index;
   //show login
   // window.location.href = "psw.html"; //jump to project page
+}
+function saveName(name) {
+  console.log("Indexjs name is " + name);
+  return name;
+}
+function savePsw(psw) {
+  console.log("Indexjs psw is " + psw);
+  return psw;
 }
